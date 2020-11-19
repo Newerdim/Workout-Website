f995b8543785b776287acde1594c8c0d7c6d03f1
Newerdim <xsilverdim@gmail.com>
2020-11-19 18:08:19 +0100

    Add prod appsettings and helper class

---
+++ Workout-Website/Helper/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workout_Website.Helper
+{
+    public class AppSettings
+    {
+        public string AccessTokenSecret { get; set; }
+        public string RefreshTokenSecret { get; set; }
+        public string ForgotPasswordTokenSecret { get; set; }
+    }
+}
