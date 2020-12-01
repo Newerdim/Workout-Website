575753fd42d4e1b7b479e6abf1c8517e9eda293e
Newerdim <xsilverdim@gmail.com>
2020-12-01 09:00:45 +0100

    Add footer to app component

--- Workout-Website/ClientApp/src/app/app.module.ts
+++ Workout-Website/ClientApp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
