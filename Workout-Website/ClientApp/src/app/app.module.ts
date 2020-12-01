eb7208006d1936ed42a5eb0ab75f377bbd1d5efd
Sebastian <newerdim@onet.pl>
2020-12-01 11:22:40 +0100

    Add header component in home

--- Workout-Website/ClientApp/src/app/app.module.ts
+++ Workout-Website/ClientApp/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/home/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, FooterComponent],
+  declarations: [AppComponent, NavComponent, FooterComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
